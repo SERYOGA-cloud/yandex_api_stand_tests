4bf45c5e4de4efb6ae97a84bfa2d926d03ce07e9
SERYOGA-cloud <gnedovsky.sergey.ekb@gmail.com>
2024-09-01 17:14:28 +0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE =  "https://55f27c7a-f6da-4c17-b592-290a3f856efa.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+        "firstName": "Naruto",
+        "lastName": "Uchiha",
+        "address": "Konoha, 142 apt.",
+        "metroStation": 4,
+        "phone": "+7 800 355 35 35",
+        "rentTime": 5,
+        "deliveryDate": "2020-06-06",
+        "comment": "Saske, come back to Konoha",
+        "color": ["BLACK"]
+    }
